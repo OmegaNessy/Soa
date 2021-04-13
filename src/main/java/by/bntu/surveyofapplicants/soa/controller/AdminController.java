2f1cecf059e090095cf0db341ea7cffc5bc177ee
OmegaNessy <kavalikhin@gmail.com>
2021-04-13 17:50:02 +0300

    Admin controller and edited DB

---
+++ src/main/java/by/bntu/surveyofapplicants/soa/controller/AdminController.java
@@ -0,0 +1,9 @@
+package by.bntu.surveyofapplicants.soa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+}
